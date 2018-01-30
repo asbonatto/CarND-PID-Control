271efcd955c19ce2769b2ed700b5a159976eaa8a
André dos Santos Bonatto <andre.s.bonatto@gmail.com>
2018-01-29 22:34:55 -0200

    Implements the pid control class

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,29 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    // Instantiates a PID controller and sets the error
+    // terms to 0
+    
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
+    
+    p_error = 0;
+    i_error = 0;
+    d_error = 0;
+    
+    
 }
 
 void PID::UpdateError(double cte) {
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error += cte;
+    
 }
 
 double PID::TotalError() {
+    // This is actually the command for the actuator
+    
+    return -(Kp*p_error + Ki*i_error + Kd*d_error);
 }
-
